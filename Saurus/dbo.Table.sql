69089c5c48f16e0646dd57e80cbfc3bae3579de5
Gustavo Peixinho Cardoso <gustavopeixinhocardoso@gmail.com>
2017-06-24 16:08:17 -0300

    commit2

---
+++ Saurus/dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Numero_interno] INT IDENTITY NOT NULL PRIMARY KEY, 
+    [CPF] VARCHAR(11) NULL, 
+    [Nome] VARCHAR(50) NULL, 
+    [Data_Nascimento] DATE NULL
+)
